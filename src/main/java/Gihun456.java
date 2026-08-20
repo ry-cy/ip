13f67e8e7cd453f097fa1de3774867aef53f5c01
ry-cy <39237291+ry-cy@users.noreply.github.com>
2026-08-20 18:12:18 +0800

    Update Gihun456.java

--- src/main/java/Gihun456.java
+++ src/main/java/Gihun456.java
@@ -1,10 +1,18 @@
 ﻿public class Gihun456 {
     public static void main(String[] args) {
-        String banner = "  ____ _ _                 _  _  ____   __   \n"
-                + " / ___(_) |__  _   _ _ __ | || || ___| / /_  \n"
-                + "| |  _| | '_ \\| | | | '_ \\| || ||___ \\| '_ \\ \n"
-                + "| |_| | | | | | |_| | | | |__   _|__) | (_) |\n"
-                + " \\____|_|_| |_|\\__,_|_| |_|  |_||____/ \\___/ \n";
+        String banner = """
+                          ____ _ _                 _  _  ____   __   
+                         / ___(_) |__  _   _ _ __ | || || ___| / /_  
+                        | |  _| | '_ \\| | | | '_ \\| || ||___ \\| '_ \\ 
+                        | |_| | | | | | |_| | | | |__   _|__) | (_) |
+                         \\____|_|_| |_|\\__,_|_| |_|  |_||____/ \\___/ 
+                        """;
         System.out.println(banner);
+
+        String greeting = "Hello! I'm Gihun456.\nWhat can I do for you?";
+        String farewell = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting);
+        System.out.println(farewell);
     }
 }
