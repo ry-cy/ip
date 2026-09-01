d64fc9fa37c40fdba11073202c8d9cce476376a6
ry-cy <39237291+ry-cy@users.noreply.github.com>
2026-09-01 10:08:55 +0800

    Add JavaDoc comments to methods

--- src/main/java/com/gihun456/model/Todo.java
+++ src/main/java/com/gihun456/model/Todo.java
@@ -1,5 +1,9 @@
 package com.gihun456.model;
 
+/**
+ * Child class of parent Task object. 
+ * Simple Todo representation without tracking of any date or time.
+ */
 public class Todo extends Task {
     public Todo(String taskName) {
         super(taskName);
